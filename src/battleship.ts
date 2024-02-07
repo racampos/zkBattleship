94b039022f42de4fbb1a6fcbd460e48cce3da3ee
Rafael Campos <racampos@gmail.com>
2024-02-07 15:14:34 -0600

    Introduces a 'Board' data structure based off a Struct to better represent the board and its methods.

--- src/battleship.ts
+++ src/battleship.ts
@@ -4,6 +4,20 @@ const toBin = (str: string) => {
   return str.replace(/██/g, '1').replace(/ {2}/g, '0');
 }
 
+class Board extends Struct({ slots: Field }) {
+  constructor(value: { slots: Field }) {
+    super(value);
+  }
+
+  isOccupied(target: Field): Bool {
+    return Gadgets.and(this.slots, target, 254).equals(Field(0)).not()
+  }
+
+  getHash(): Field {
+    return Poseidon.hash([this.slots]);
+  }
+}
+
 class TargetAndBoardCommitment extends Struct({ target: Field, boardCommitment: Field}) {
   constructor(value: { target: Field, boardCommitment: Field }) {
     super(value);
@@ -17,19 +31,20 @@ const HitOrMiss = ZkProgram({
 
   methods: {
     run: {
-      privateInputs: [Field], // board
+      privateInputs: [Board],
 
-      method(publicInput: TargetAndBoardCommitment, board: Field): Bool {
+      method(publicInput: TargetAndBoardCommitment, board: Board): Bool {
         // Validate that the board commitment matches the board
-        publicInput.boardCommitment.assertEquals(Poseidon.hash([board]), "board must match the previously commited board");
+        publicInput.boardCommitment.assertEquals(board.getHash(), "board must match the previously commited board");
         // Validate that target is not 0
         publicInput.target.assertNotEquals(Field(0), "target must not be 0");
         // Validate that target contains a single 1
         Gadgets.and(publicInput.target, publicInput.target.sub(1), 254).assertEquals(Field(0), "target must contain a single 1");
 
         // Determine if the target is a hit or miss
-        const result = Gadgets.and(board, publicInput.target, 254);
-        return result.equals(Field(0)).not();
+        // const result = Gadgets.and(board, publicInput.target, 254);
+        // return result.equals(Field(0)).not();
+        return board.isOccupied(publicInput.target);
       },
     },
   }
@@ -70,7 +85,7 @@ const boardStr = ( '                    ' +
                    '      ██      ██    ' );
 
 const boardBin = BigInt('0b' + toBin(boardStr));
-const board = Field.from(boardBin);
+const board = new Board( {slots: Field.from(boardBin) });
 
 const targetStr = ( '                    ' +
                     '                    ' + 
@@ -86,7 +101,7 @@ const targetStr = ( '                    ' +
 const targetBin = BigInt('0b' + toBin(targetStr));
 const targetField = Field.from(targetBin);
 
-const target = new TargetAndBoardCommitment({ target: targetField, boardCommitment: Poseidon.hash([board]) });
+const target = new TargetAndBoardCommitment({ target: targetField, boardCommitment: board.getHash() });
 
 const hitOrMiss = await HitOrMiss.run(target, board);
 const end = performance.now();
